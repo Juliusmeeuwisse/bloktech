e406d4c9dd8182bfa138727de768a17ca974bf85
juliusmeeuwisse <juliusmeeuwisse@gmail.com>
2021-04-26 13:18:30 +0200

    nieuwe versie

--- server.js
+++ server.js
@@ -9,12 +9,20 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
+app.get('/login', (req, res) => {
+  res.send('The login to account page')
+});
+
+app.get('/signin', (req, res) => {
+  res.send('The create account page')
+});
+
 app.get('/muziek', (req, res) => {
   res.send('Hello World!')
 });
 
 app.use(function(req, res, next ) {
-  res.status(404).send("Sorry can't find that!")
+  res.status(404).send("I'm sorry but we couldn't find that page!")
 });
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
