62fdaca33ea26689d406f90d0042eed975da94c2
juliusmeeuwisse <juliusmeeuwisse@gmail.com>
2021-05-18 00:01:43 +0200

    worked on index.ejs, server.js and likelist.ejs

--- server.js
+++ server.js
@@ -1,36 +1,87 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const dotenv = require("dotenv")
+dotenv.config();
 
-app.set('view engine', 'ejs');
+let currentUser = [];
+let likedList = [];
+let dislikedList = [];
+
+
+let profile = {
+  firstname: "testsubject",
+  age: 19,
+}
+
+const users = [{
+    "userID": 0,
+    "firstname": "Pieter",
+    "age": 20,
+    "Muziekgenre": "rap"
+  },
+  {
+    "userID": 1,
+    "firstname": "Jaap",
+    "age": 23,
+    "Muziekgenre": "pop"
+  }
+
+]
+
+getUserProfile = (userID) => {
+  console.log('User profile ID ${userID}')
+  return users[userID - 1];
+}
+AvailableUser = () => {
+  for (let i = 0; i < users.length; i++) {
+    if (
+      !likedList.includes(users[i]) &&
+      !dislikedList.includes(users[i])
+    ) {
+      return users[i]
+    }
+  }
+  return null;
+}
 
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded());
-app.set ('view engine' , 'ejs');
+app.set('view engine', 'ejs');
 
-let music = ["Rap" , "R&B", "Pop"]
-
+//Renders main page.
 app.get('/', (req, res) => {
-  let musicrandom = music[Math.floor(Math.random() * music.length)];
-  res.render('index', {musicrandom});
+  currentUser = AvailableUser();
+  res.render('index', {
+    user: currentUser
+  });
 });
 
 app.get('/matches', (req, res) => {
   res.send('The main page with all matches')
 });
 
-app.get('/likedlist', (req, res) => {
-  res.send('List with all your likes')
+app.get('/likelist', (req, res) => {
+  res.render('likelist')
 });
 
-app.get('/muziek', (req, res) => {
-  res.send('Hello World!')
+app.get('/profile', (req, res) => {
+  res.render('profilepage')
 });
 
-app.use(function(req, res, next ) {
+
+app.post('/like', (req, res) => {
+  console.log("hi")
+  likedList.push(currentUser)
+  res.redirect('/')
+  currentUser = AvailableUser();
+});
+
+app.use(function (req, res, next) {
   res.status(404).send("I'm sorry but we couldn't find that page!")
 });
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
