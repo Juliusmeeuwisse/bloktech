021e3b07414af66cfa28c42557c7265b500b9923
juliusmeeuwisse <juliusmeeuwisse@gmail.com>
2021-05-19 02:16:34 +0200

    added final CSS and fixed like feature

--- server.js
+++ server.js
@@ -1,51 +1,51 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const dotenv = require("dotenv")
 dotenv.config();
 
+//Variables for like feature
 let likedList = [];
 let dislikedList = [];
 let currentUser = [];
 
-console.log(likedList);
-
-let profile = {
-  firstname: "testsubject",
-  age: 19,
-}
-
+//Fake database, will change to mongoDB 
 const users = [{
     "userID": 0,
     "firstname": "Pieter",
     "age": 20,
+    "profilepicture": 'images/profilepicture1.jpeg',
     "musicGenre": "rap"
   },
   {
     "userID": 1,
     "firstname": "Jaap",
     "age": 23,
+    "profilepicture": 'images/profile_img.png',
     "musicGenre": "pop"
+
   },
   {
     "userID": 2,
-    "firstname": "geert",
-    "age": 30,
+    "firstname": "Jip",
+    "age": 25,
+    "profilepicture": '/images/profilepicture2.jpeg',
+    "musicGenre": "pop"
+  },
+  {
+    "userID": 3,
+    "firstname": "Geert",
+    "age": 31,
+    "profilepicture": '/images/profilepicture3.jpeg',
     "musicGenre": "pop"
   }
 
 ]
-
-// let getUserProfile = (userID) => {
-//   console.log('User profile ID ${userID}')
-//   return users[userID - 1];
-// }
-
-let AvailableUser = () => {
+// function that checks what user is available from users and isnt included in likedList nor dislikedList
+let availableUser = () => {
   for (let i = 0; i < users.length; i++) {
     if (
-      !likedList.includes(users[i]) &&
-      !dislikedList.includes(users[i])
+      !likedList.includes(users[i]) && !dislikedList.includes(users[i])
     ) {
       return users[i]
     }
@@ -58,40 +58,46 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.set('view engine', 'ejs');
 
-//Renders main page.
+// renders main page.
 app.get('/', (req, res) => {
-  currentUser = AvailableUser();
+  currentUser = availableUser();
   res.render('index', {
     user: currentUser
-  }); 
+  });
 });
 
-app.get('/matches', (req, res) => {
-  res.send('The main page with all matches')
-});
-
+// renders like list
 app.get('/likelist', (req, res) => {
   res.render('likelist', {
     liked: likedList
   })
 });
 
+// renders profile page 
 app.get('/profile', (req, res) => {
   res.render('profilepage')
 });
 
-
+// click on likebutton > pushes currentuser to likedlist
 app.post('/like', (req, res) => {
-  likedList.push(currentUser)
-  currentUser = AvailableUser();
+  likedList.push(currentUser);
+  currentUser = availableUser();
   res.render('index', {
     user: currentUser
   });
 });
 
-console.log(likedList);
+// click on dislikebutton > pushes currentuser to dislikelist
+app.post('/dislike', (req, res) => {
+  dislikedList.push(currentUser);
+  currentUser = availableUser();
+  res.render('index', {
+    user: currentUser
+  });
+});
 
-app.use(function (req, res, next) {
+// error message when typed in wrong path
+app.use(function (req, res) {
   res.status(404).send("I'm sorry but we couldn't find that page!")
 });
 
